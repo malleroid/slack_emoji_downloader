9622a3a9065e48a58f76389ce291d9824bd50198
malleroid <veldiata.mig@gmail.com>
2022-02-18 15:42:44 +0900

    refact: redefine funcitons

--- script.py
+++ script.py
@@ -1,13 +1,26 @@
 import json
 import requests
 
-file = open('./emoji.json', 'r')
-data = json.load(file)
 
-targets = data['emoji']
+def main():
+    targets = load_json()
+    download_emoji(targets)
 
-for emoji in targets:
-    response = requests.get(emoji['url']).content
 
-    with open('./tmp/' + emoji['name'] + '.png', 'wb') as f:
-        f.write(response)
+def load_json():
+    file = open('./emoji.json', 'r')
+    data = json.load(file)
+
+    return data['emoji']
+
+
+def download_emoji(target_array):
+    for target in target_array:
+        response = requests.get(target['url']).content
+
+        with open('./tmp/' + target['name'] + '.png', 'wb') as f:
+            f.write(response)
+
+
+if __name__ == "__main__":
+    main()
